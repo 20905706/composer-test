9a204e8e986ad9d8db705376e6220927961de682
Zhang Li <20905706@qq.com>
2018-12-05 10:47:26 +0800

    in order to build my packagist

---
+++ src/Test/Hello/Hello.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Test\Hello;
+/**
+ * Created by PhpStorm.
+ * User: zl
+ * Date: 2018/12/5
+ * Time: 10:32
+ */
+class Hello
+{
+    public static function info()
+    {
+        echo "hello ";
+    }
+}
---
+++ src/Test/World/World.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Test\World;
+/**
+ * Created by PhpStorm.
+ * User: zl
+ * Date: 2018/12/5
+ * Time: 10:33
+ */
+class World
+{
+    public static function info()
+    {
+        echo "world!";
+    }
+}
